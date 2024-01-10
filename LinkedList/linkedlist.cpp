66f5f4c823e5088549387b37e4d4837d4d5dfdb2
gincode18 <149682346+vishalcredain@users.noreply.github.com>
2024-01-10 11:33:57 +0530

    :)

---
+++ LinkedList/linkedlist.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        this->data = x;
+        next = NULL;
+    }
+};
+void push(Node *&head, int x)
+{
+    Node *temp = new Node(x);
+    temp->next = head;
+    head=temp;
+}
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *x = new Node(2);
+    push(x, 4);
+    push(x, 3);
+    push(x, 2);
+    push(x, 1);
+    print(x);
+    return 0;
+}
